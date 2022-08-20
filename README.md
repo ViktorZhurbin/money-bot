f41911a67bb6f9c76f6de5588f479d524351ac38
Viktor Zhurbin <vzhurbin@gmail.com>
2022-08-20 19:35:14 +0200

    refactor

---
+++ README.md
@@ -0,0 +1,7 @@
+After deployemnt, you need to set up webhook. Replace `<YOUR-BOT-TOKEN>` and `<YOUR-PROJECT-URL>` with the proper values.
+
+```
+curl -X POST https://api.telegram.org/bot<YOUR-BOT-TOKEN>/setWebhook \
+  -H "Content-type: application/json" \
+  -d '{"url": "<YOUR-PROJECT-URL>/api/webhook"}'
+```
--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,21 @@
-import { webhookCallback } from "grammy";
-import { bot } from "../src/bot";
+import "dotenv/config";
+
+import { Bot, webhookCallback } from "grammy";
+
+const { TOKEN } = process.env;
+
+if (!TOKEN) {
+  console.log("Error: TOKEN not found");
+
+  process.exit();
+}
+
+const bot = new Bot(TOKEN);
+
+// attach all middleware
+bot.on("message", async (ctx) => {
+  await ctx.reply("Hi there!");
+});
 
 // The free version of vercel has restrictions on quotas, which we need to enable in the configuration file vercel.json
 // webhookCallback will make sure that the correct middleware(listener) function is called
